339a91065543723399f1dc2ea8d2dcb032fc649c
JianJieZhen <116881298+JianJieZhen@users.noreply.github.com>
2024-06-01 20:37:39 +0800

    Update Sign_Up.php

--- Sign_Up.php
+++ Sign_Up.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // 密碼加密
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO users (staff_id, password) VALUES (?, ?)";
+    $sql = "INSERT INTO user (staff_id, password) VALUES (?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ss", $staff_id, $hashed_password);
 
@@ -35,33 +35,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $conn->close();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign up</title>
-</head>
-<body>
-    <div class="container">
-        <form method="POST" action="Sign_up.php">
-            <div class="form-group">
-                <p>
-                    Please enter the staff ID and default password
-                </p>
-            </div>
-            <div class="form-group">
-                <label for="staff_id">Staff ID: </label>
-                <input type="text" id="staff_id" name="staff_id" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password: </label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit" class="submitbtn">Register</button>
-        </form>
-    </div>
-</body>
-</html>
