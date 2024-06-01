193ac869bd12db16fbedc2386d226223150a7e47
JianJieZhen <116881298+JianJieZhen@users.noreply.github.com>
2024-06-01 20:47:04 +0800

    .

--- Sign_Up.php
+++ Sign_Up.php
@@ -3,7 +3,7 @@
 $servername = "localhost"; // MySQL 伺服器主機名稱
 $username = "root"; // MySQL 使用者名稱
 $password = ""; // MySQL 密碼
-$dbname = "DBMS_Project"; // 資料庫名稱
+$dbname = "dbms_project"; // 資料庫名稱
 
 // 建立連線
 $conn = new mysqli($servername, $username, $password, $dbname);
