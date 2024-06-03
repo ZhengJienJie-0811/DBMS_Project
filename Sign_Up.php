01774e3e3ef2015644c9e12f2627824fbaa4f345
JianJieZhen <116881298+JianJieZhen@users.noreply.github.com>
2024-06-03 17:16:36 +0800

    .

--- Sign_Up.php
+++ Sign_Up.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($stmt->execute()) {
         echo "註冊成功";
-        header('Location:index.html');
+        
     } else {
         echo "錯誤: " . $sql . "<br>" . $conn->error;
     }
